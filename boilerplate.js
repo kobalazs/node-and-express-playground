af23da611b277800a76fc29df066142170feb879
Balázs Kovács <balazs.kovacs@stylersonline.com>
2020-05-07 21:50:14 +0200

    Database connection boilerplate code

---
+++ boilerplate.js
@@ -0,0 +1,28 @@
+const console = require('console');
+const fs = require('fs');
+const sqlite3 = require('sqlite3').verbose();
+
+const DB_PATH = 'database/database.sqlite';
+
+fs.writeFile(DB_PATH, '', { flag: 'a' }, (err) => {
+  if (err) {
+    throw err;
+  }
+});
+const db = new sqlite3.Database(DB_PATH);
+
+db.serialize(() => {
+  db.run('CREATE TABLE IF NOT EXISTS lorem (info TEXT)');
+
+  const stmt = db.prepare('INSERT INTO lorem VALUES (?)');
+  for (let i = 0; i < 10; i++) {
+    stmt.run(`Ipsum ${i}`);
+  }
+  stmt.finalize();
+
+  db.each('SELECT rowid AS id, info FROM lorem', (err, row) => {
+    console.log(`${row.id}: ${row.info}`);
+  });
+});
+
+db.close();
